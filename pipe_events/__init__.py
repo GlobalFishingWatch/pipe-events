bc1dbbf391c99ace11510c410767f3b8b5e6b632
Andres Arana <and2arana@gmail.com>
2018-11-21 16:25:05 -0300

    Bumps pipe events version

--- pipe_events/__init__.py
+++ pipe_events/__init__.py
@@ -2,7 +2,7 @@
 Pipeline for publishing summarized event information
 """
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Andres Arana'
 __email__ = 'andres@globalfishingwatch.org'
 __source__ = 'https://github.com/GlobalFishingWatch/pipe-events'
