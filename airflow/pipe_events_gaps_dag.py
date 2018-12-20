c1bdfee8e1579b8c14fc4db572e414f6edfa0458
Andres Arana <and2arana@gmail.com>
2018-12-20 11:50:29 -0300

    Adds unified gap events to the postgres database

--- airflow/pipe_events_gaps_dag.py
+++ airflow/pipe_events_gaps_dag.py
@@ -26,8 +26,8 @@ class PipelineDagFactory(DagFactory):
         with DAG(dag_id, schedule_interval=self.schedule_interval, default_args=self.default_args) as dag:
             source_sensors = self.source_table_sensors(dag)
 
-            publish_events = BashOperator(
-                task_id='publish_events',
+            publish_events_bigquery = BashOperator(
+                task_id='publish_events_bigquery',
                 pool='bigquery',
                 bash_command='{docker_run} {docker_image} generate_gap_events '
                              '{date_range} '
@@ -39,7 +39,19 @@ class PipelineDagFactory(DagFactory):
                                  **config)
             )
 
+            publish_events_postgres = BashOperator(
+                task_id='publish_events_postgres',
+                bash_command='{docker_run} {docker_image} publish_postgres '
+                '{date_range} '
+                '{project_id}:{events_dataset}.{events_table} '
+                '{temp_bucket} '
+                '{postgres_instance} '
+                '{postgres_connection_string} '
+                '{postgres_table} '
+                'gap'.format(**config)
+            )
+
             for sensor in source_sensors:
-                dag >> sensor >> publish_events
+                dag >> sensor >> publish_events_bigquery >> publish_events_postgres
 
             return dag
