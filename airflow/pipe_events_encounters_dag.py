e109dc639c71ea1d2e399c61df3ef05b4711b870
Andres Arana <and2arana@gmail.com>
2018-12-20 15:38:12 -0300

    Adds encounter events to the postgis import

--- airflow/pipe_events_encounters_dag.py
+++ airflow/pipe_events_encounters_dag.py
@@ -10,8 +10,8 @@ class PipelineDagFactory(DagFactory):
         config['date_range'] = ','.join(self.source_date_range())
 
         with DAG(dag_id, schedule_interval=self.schedule_interval, default_args=self.default_args) as dag:
-            publish_events = BashOperator(
-                task_id='publish_events',
+            publish_events_bigquery = BashOperator(
+                task_id='publish_events_bigquery',
                 pool='bigquery',
                 bash_command='{docker_run} {docker_image} generate_encounter_events '
                              '{project_id}:{source_dataset}.{source_table} '
@@ -19,6 +19,18 @@ class PipelineDagFactory(DagFactory):
                                  **config)
             )
 
-            dag >> publish_events
+            publish_events_postgres = BashOperator(
+                task_id='publish_events_postgres',
+                bash_command='{docker_run} {docker_image} publish_postgres '
+                '{date_range} '
+                '{project_id}:{events_dataset}.{events_table} '
+                '{temp_bucket} '
+                '{postgres_instance} '
+                '{postgres_connection_string} '
+                '{postgres_table} '
+                'encounter'.format(**config)
+            )
+
+            dag >> publish_events_bigquery >> publish_events_postgres
 
             return dag
