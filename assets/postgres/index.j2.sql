065adccb27333a2224bc2cf810ab31ff7049550b
aperdizs <info@alvaroperdiz.com>
2021-04-21 12:21:37 +0200

    Create new asset and update others to avoid errors

--- assets/postgres/index.j2.sql
+++ assets/postgres/index.j2.sql
@@ -3,8 +3,8 @@ CREATE INDEX {{ table_name }}_event_id ON public.{{ table_name }} (event_id);
 CREATE INDEX {{ table_name }}_event_type ON public.{{ table_name }} (event_type);
 CREATE INDEX {{ table_name }}_event_start ON public.{{ table_name }} (event_type, event_start);
 CREATE INDEX {{ table_name }}_vessel_id ON public.{{ table_name }} (event_type, vessel_id);
-CREATE INDEX {{ table_name }}_event_geography_gis ON public.{{ table_name }} USING gist (event_geography);
-CREATE INDEX {{ table_name }}_event_mean_position_gis ON public.{{ table_name }} USING gist (event_mean_position);
+#CREATE INDEX {{ table_name }}_event_geography_gis ON public.{{ table_name }} USING gist (event_geography);
+#CREATE INDEX {{ table_name }}_event_mean_position_gis ON public.{{ table_name }} USING gist (event_mean_position);
 
 -- Ensure we run vacuumming after deleting most of the rows in the table
 VACUUM ANALYZE public.{{ table_name }};
--- assets/postgres/setup.j2.sql
+++ assets/postgres/setup.j2.sql
@@ -1,15 +1,15 @@
 -- Create base events table
 CREATE TABLE IF NOT EXISTS public.{{ table_name }} (
-  id bigserial PRIMARY KEY NOT NULL,
-  event_id character varying NOT NULL,
+  /*id bigserial PRIMARY KEY NOT NULL,*/
+  event_id character varying PRIMARY KEY NOT NULL,
   event_type character varying NOT NULL,
   vessel_id character varying NOT NULL,
   event_start timestamp without time zone NOT NULL,
   event_end timestamp without time zone,
   event_info jsonb NOT NULL,
-  event_vessels jsonb NOT NULL,
+  event_vessels jsonb NOT NULL/*,
   event_geography public.geography(MultiPoint,4326) NOT NULL,
-  event_mean_position public.geography(Point, 4326) NOT NULL
+  event_mean_position public.geography(Point, 4326) NOT NULL*/
 );
 
 -- Ensure the table is empty
