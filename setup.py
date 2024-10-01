eaef20e4273b60c1473d8967b398b78ddd82aa93
Matias Piano <matias@globalfishingwatch.org>
2024-09-30 22:05:30 -0300

    Include dependencies inside the requirements of the project, update setup.py and add tests

--- setup.py
+++ setup.py
@@ -9,12 +9,16 @@ from setuptools import setup
 
 setup(
     name='pipe-events',
-    version='4.1.1',
+    version='4.1.2',
     author="Global Fishing Watch.",
+    description=(
+        "Pipeline for publishing summarized event information"
+    ),
     url="https://github.com/GlobalFishingWatch/pipe-events",
     packages=find_packages(exclude=['test*.*', 'tests']),
     install_requires=[
         'jinja2-cli<1',
+        'google-cloud-bigquery<4',
     ],
 )
 
