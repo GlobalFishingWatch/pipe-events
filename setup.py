29723369dcdb445bf0594be9da7c2366b315b798
Matias Piano <matias@globalfishingwatch.org>
2024-10-18 10:08:27 -0300

    Updates the project version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='pipe-events',
-    version='4.1.2',
+    version='4.2.0',
     author="Global Fishing Watch.",
     description=(
         "Pipeline for publishing summarized event information"
