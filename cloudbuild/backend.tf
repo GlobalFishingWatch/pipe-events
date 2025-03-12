4d2655bb34e81904a2a1f325ff5ef16eb4a40cbb
Raul <rrequero@gmail.com>
2025-03-12 17:49:21 +0100

    update terraform

--- cloudbuild/backend.tf
+++ cloudbuild/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "gfw-int-infrastructure-tfstate-us-central1"
-    prefix = "cloudbuild-gfw-tool" # Not change for this project
+    prefix = "cloudbuild-pipe-events" # Not change for this project
   }
 }
