29ee387ffb265feeb53a3fb6eba37345b429af4d
Matias Piano <matias@globalfishingwatch.org>
2025-06-11 16:47:23 -0300

    Revert "Increments project version"
    This reverts commit 3aa023cce486d041cf5ec48bec5ddd781eaf8f6c.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='pipe-events',
-    version='5.0.0',
+    version='4.2.7',
     author="Global Fishing Watch.",
     description=(
         "Pipeline for publishing summarized event information"
