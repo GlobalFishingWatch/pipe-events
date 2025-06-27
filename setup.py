802ee5ed4e9f73b8b6a6a66f065261ae128a2fff
Matias Piano <matias@globalfishingwatch.org>
2025-06-27 10:10:55 -0300

    Bumps version to v4.2.10

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='pipe-events',
-    version='4.2.7',
+    version='4.2.10',
     author="Global Fishing Watch.",
     description=(
         "Pipeline for publishing summarized event information"
